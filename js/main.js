f90cb8658f9835aca563fbcf9ce12f13a483e014
Derrick <derrickejwallace@gmail.com>
2018-06-12 16:47:15 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,33 +1,113 @@
+//take list of HTML students 
+//calculate # of students in the list, give each a number and save as const
+//calculate # of pages based on number of students in the list and save as a const
+//grab each li item with studetnt-info compare its numerical const and if < 9 page = page 1
+//if the numerical const is >10<20 = pg2 ...etc
+//print to page
+//display buttons number  based on the const pages
+
 //show page function~~ builds a list of 10 students and displays on page.  
 
 
-const numbStudents = $('.student-list .student-item').length;
-const limitPerPage = 10;
-const pages = Math.ceil(numbStudents / limitPerPage);
+
+const students = $(".student-list");
+    $('.student-item').hide();
+   // console.log(students);
+const studentsPerPage = 10;
+const numberOfPages = Math.ceil(students.length / studentsPerPage);
+const page = $('.page');
+console.log(page);
+
+for (let i = 0; i < students.length; i++) {
+ 
+    const chunk = $(".student-item").slice( i );
+    (chunk).show();
+    console.log(chunk);
+}  
+ 
+//paginiation function
 
 
-alert('numb of students' + numbStudents + ' ' + 'pages' + pages);
-console.log('numb of students' + numbStudents + ' ' + 'pages' + pages);
+
+// const students = $(".student-item");
+//     $('.student-item').hide();
+//     console.log(students);
+// const studentsPerPage = 10;
+// 
+// 
+// console.log(numberOfPages);
+
+// function arrayChunker(students) {
+// //function pageNumber(students * 10 + 1) {}
+// for (let i = 0; i <= 10 || students.length; i ++) { 
+//     const chunk = $(".student-item").slice( i );
+        
+//     (chunk).show();
+//     console.log(chunk);
+// }  
 
 
-const hideList = (list) => {
-for (let i = 0; i < list.length; i++) {
-    //go through the student-list with i
-    $('.student-list').hide();
-    console.log(i);
-    if (i < 10) {
-                //display on page 1
-                $('.student-item').show();
-                alert(i);
-            } else {
-                //hide
-                $('.student-item').hide();
-            }
-        }
 
-}
+// //console.log(students);
+
+// }
+// //arrayChunker (students);
 
 
+
+// const students = $(".student-item");
+//     $('.student-item').hide();
+//     console.log(students);
+//  const numberOfPages = Math.ceil(students.length / studentsPerPage);
+//     console.log('# of pages:' + numberOfPages);
+
+// for (let i = 0; i < students.length; i++) {  
+//     const chunk = $(".student-item").slice( i - 10);
+//     (chunk).show();
+//     console.log(chunk);
+// } 
+ //}
+ //for (let i = 0; i < students.list; i++) {
+ //}
+
+// perPage = 10;
+// const pages = Math.ceil(numbStudents / limitPerPage);
+
+// function Paginator(items, page, per_page) {
+ 
+//     var page = page || 1,
+//     per_page = per_page || 10,
+//     offset = (page - 1) * per_page,
+   
+//     paginatedItems = items.slice(offset).slice(0, per_page),
+//     total_pages = Math.ceil(items.length / per_page);
+//     return {
+//     page: page,
+//     per_page: per_page,
+//     pre_page: page - 1 ? page - 1 : null,
+//     next_page: (total_pages > page) ? page + 1 : null,
+//     total: items.length,
+//     total_pages: total_pages,
+//     data: paginatedItems
+//     };
+//   }
+
+// const hideList = (students) => {
+// for (let i = 0; i < students.length; i++) {
+//     //go through the student-list with i
+//     $('.student-list').hide();
+//     console.log(i);
+//     if (i < 10) {
+//                 //display on page 1
+//                 $('.student-item').show();
+//                 alert(i);
+//             } else {
+//                 //hide
+//                 $('.student-item').hide();
+//             }
+//         }
+
+// }
 
 //$(".student-list .student-item:gt(' limitPerPage ')").hide();
 
